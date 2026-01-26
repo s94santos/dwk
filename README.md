8b9e502c53c721c5104e66af9ff78c4ae2db579c
ssantos <s94santos@gmail.com>
2026-01-26 23:09:17 +0000

    Add exercise 2.7

--- README.md
+++ README.md
@@ -23,3 +23,4 @@
 - [2.4.](https://github.com/s94santos/dwk/tree/2.4/the_project)
 - [2.5.](https://github.com/s94santos/dwk/tree/2.5/log_output)
 - [2.6.](https://github.com/s94santos/dwk/tree/2.6/the_project)
+- [2.7.](https://github.com/s94santos/dwk/tree/2.7/ping-pong)
--- ping-pong/main.go
+++ ping-pong/main.go
@@ -2,23 +2,60 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"log"
+	"context"
+	"os"
 )
+
+func getCounterFromDB(conn *pgx.Conn) (int, error) {
+	var value int
+	err := conn.QueryRow(context.Background(), "SELECT value FROM counter WHERE name = 'main'").Scan(&value)
+	if err != nil {
+		return 0, err
+	}
+	return value, nil
+}
+
+func setCounterInDB(conn *pgx.Conn, value int) error {
+	_, err := conn.Exec(context.Background(), "UPDATE counter SET value=$1 WHERE name='main'", value)
+	return err
+}
 
 func main() {
 
-	cnt := 0
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	defer conn.Close(context.Background())
+
+
 	r := gin.Default()
 
 	r.GET("/pingpong", func(c *gin.Context) {
+		cnt, err := getCounterFromDB(conn)
+		if err != nil {
+			log.Fatal(err)
+		}
 		cnt++
+		err = setCounterInDB(conn, cnt)
+		if err != nil {
+			log.Fatal(err)
+		}
 		c.JSON(200, gin.H{
 			"counter": cnt,
 		})
 	})
 
 	r.GET("/ping", func(c *gin.Context) {
+		value, err := getCounterFromDB(conn)
+		if err != nil {
+			log.Fatal(err)
+		}
 		c.JSON(200, gin.H{
-			"counter": cnt,
+			"counter": value,
 		})
 	})
 
